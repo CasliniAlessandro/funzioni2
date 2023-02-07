58ad2beeba3bf251a84961453aa3afc75c85a09c
Alessandro Caslini <caslini.alessandro.studente@itispaleocapa.it>
2023-02-07 16:29:52 +0100

    funzione1

--- funzioni2/Program.cs
+++ funzioni2/Program.cs
@@ -28,7 +28,7 @@ namespace menù
                 Console.WriteLine("5- VISUALIZZA ANIMALI RIPETUTI: ");
                 Console.WriteLine("6- MODIFICA NOME: ");
                 Console.WriteLine("7- VISUALIZZAZIONE ANIMALI");
-                Console.WriteLine("8- RICERCA NOME PIU' LUNGO E PIU'");
+                Console.WriteLine("8- RICERCA NOME PIU' LUNGO E PIU' CORTO ");
                 Console.WriteLine("9- CANCELLA TUTTI I NOMI UGUALI");
 
                 sceltamenù = int.Parse(Console.ReadLine());
@@ -77,13 +77,29 @@ namespace menù
                         break;
 
                     case 5:
-
+                        Console.WriteLine(funzione5(vet, lun));
                         break;
 
 
                     case 6:
-                        ModificaNome();
+                        ModificaNome(vet);
                         break;
+
+                    case 7:
+                        VisualizzaAnimali(vet);
+                        break;
+
+                    case 8:
+                        Console.WriteLine(funzione8(vet, animale));
+                        break;
+
+                    case 9:
+                        Console.WriteLine("Inserisci l'elemento da eliminare");
+                        string el = Console.ReadLine();
+
+                        funzione9(vet, el);
+                        break;
+
 
 
 
@@ -159,9 +175,50 @@ namespace menù
             return -1;
 
         }
-        static void funzione5()
+        static void funzione5(int lun, string[] vet)
         {
-
+            int[] occorrenze = new int[lun];
+            string[] uni = new string[lun];
+            string outp = "";
+            for (int i = 0; i < lun; i++)
+            {
+                uni[i] = vet[i];
+            }
+            int x, y;
+            string temp;
+            for (x = 0; x < lun - 1; x++)
+            {
+                for (y = 0; y < lun - 1; y++)
+                {
+                    int comp = string.Compare(uni[y], uni[y + 1]);
+                    if (comp == 1)
+                    {
+                        temp = uni[y];
+                        uni[y] = uni[y + 1];
+                        uni[y + 1] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < lun; i++)
+            {
+                if (uni[i] != "")
+                {
+                    occorrenze[i] = 1;
+                    for (int j = i + 1; j < lun; j++)
+                    {
+                        if (uni[i] == uni[j])
+                        {
+                            uni[j] = "";
+                            occorrenze[i]++;
+                        }
+                    }
+                    if (occorrenze[i] > 1)
+                    {
+                        outp += ($"{uni[i]}: {occorrenze[i]}");
+                    }
+                }
+            }
+            return outp;
         }
         static void ModificaNome(ref string[] vet, string animale, string parolaGiusta)
         {
@@ -186,13 +243,36 @@ namespace menù
                 }
             }
         }
-        static void funzione8()
+        static void funzione8(string[]arr, int lun)
         {
-
+            string lungo = arr[0], corto = arr[0];
+            string outp = "";
+            for(int i = 1; i < lun; i++)
+            {
+                if (arr[i].Length > lungo.Length)
+                {
+                    lungo = arr[i];
+                }
+                if (arr[i].Length < lungo.Length)
+                {
+                    corto = arr[i];
+                }
+            }         
+            outp += ($"Il nome più lungo è:{lungo}\n il nome più corto è:{corto}");
+            return outp;
         }
-        static void funzione9()
+        static void funzione9(string[]a, string e)
         {
-
+            for(int g=0;g<a.Length;g++)
+            {
+                if (a[g].ToLower() == e.ToLower())
+                {
+                    for(int j = g; j < 99; j++)
+                    {
+                        a[j] = a[j + 1];
+                    }
+                }
+            }
         }
     }
 }
